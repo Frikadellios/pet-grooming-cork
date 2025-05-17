9ae0c96e8d7d0ddfd22ce0a249e3f48ca441de54
Hrihorii Ilin <frikadellios@gmail.com>
2025-05-17 20:32:56 +0100

    remove sh

--- plugins/plugin.js
+++
@@ -1,9 +0,0 @@
-export default function ({ addVariant }) {
-  addVariant('inverted', '@media (inverted-colors: inverted)')
-  addVariant('hocus', ['&:focus', '&:hover'])
-}
-
-module.exports = ({ addVariant }) => {
-  addVariant('inverted', '@media (inverted-colors: inverted)')
-  addVariant('hocus', ['&:focus', '&:hover'])
-}
