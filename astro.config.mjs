9da3a1e1333687b7c788c010d065f2f335cf0605
Hrihorii Ilin <frikadellios@gmail.com>
2025-05-17 19:59:41 +0100

    add some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()]
-	},
+    },
 
-  integrations: [react()]
+  integrations: [react(), mdx(), sitemap()]
 });
